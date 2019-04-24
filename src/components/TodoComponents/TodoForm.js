678ba1ba57a2f29ab2dd64c0c68255cd9484cbe8
victorN <victornguyen082@gmail.com>
2019-04-24 16:26:39 -0400

    React Todo Test

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Todo.css';
+
+function TodoForm() {
+    return (
+        <div>
+            <form>
+                <input placeholder="To do" />
+            </form>
+        </div>
+    )
+}
+
+export default TodoForm;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,14 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import './Todo.css';
+
+function TodoList() {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    )
+}
+
+export default TodoList;
